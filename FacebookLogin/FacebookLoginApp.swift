6e0388b7968ff17ee93c2577c1daaeb396359e81
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-04-16 11:33:17 -0300

    Initial Commit

---
+++ FacebookLogin/FacebookLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookLoginApp.swift
+//  FacebookLogin
+//
+//  Created by Pedro Marques on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FacebookLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
